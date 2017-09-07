9f89d50af9e882d62f7527564d3fb957f264accf
kamontat <kamontat_c@hotmail.com>
2017-09-07 23:16:58 +0700

    fixed install script

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ if [ ! -d ~/.github ]; then
   mkdir ~/.github
 fi
 
-cp -rf *.md ~/.github/.
+cp -rf * ~/.github/.
+
+rm -rf ~/.github/README.md ~/.github/install.sh ~/.github/.git*
 
 echo "Complete: check on ~/.github/ folder"
